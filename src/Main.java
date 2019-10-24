fbf9c4b67dd7341d6ebbe115cd3bd09521c49dcb
Gary M <50461559+aSilverSoul@users.noreply.github.com>
2019-10-23 22:42:10 -0700

    update

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+import static java.lang.Math.*;
+
 public class Main {
-    private static final double EPSILON = 1e-10;
+    public static void main(String[] args) throws IOException {
+
+        int n;
+        double[][] matrixCreated;
+        double[] matrixB;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter the number of variables in the equation:");
+        n = Integer.parseInt(reader.readLine());
+        matrixCreated= new double[n][n];
+        matrixB=new double[n];
+        System.out.println("Enter the matrix:");
+
+        for (int i = 0; i < n; i++)
+        {
+            StringTokenizer matrixToken = new StringTokenizer(reader.readLine());
+
+            while (matrixToken.hasMoreTokens())
+                for (int j = 0; j < n && matrixToken.hasMoreTokens(); j++)
+                    matrixCreated[i][j] = Integer.parseInt(matrixToken.nextToken());
+        }
+
+        System.out.println("enter the right side of the equations");
+        StringTokenizer results=new StringTokenizer(reader.readLine());
+        while(results.hasMoreTokens()){
+            for(int k=0;k<n;k++)
+                matrixB[k]=Integer.parseInt(results.nextToken());
+        }
+
+        double[] x = lsolve(matrixCreated, matrixB);//solve the matrix given
+
+
+        // print results
+        for (int i = 0; i < n; i++) {
+            System.out.println(x[i]);
+        }
+
+    }
 
     // Gaussian elimination with partial pivoting
-    public static double[] lsolve(double[][] A, double[] b) {
-        int n = b.length;
+    public static double[] lsolve(double[][] mainMatrix, double[] rightSide) {
+        int n = rightSide.length;
 
         for (int p = 0; p < n; p++) {
 
             // find pivot row and swap
             int max = p;
             for (int i = p + 1; i < n; i++) {
-                if (Math.abs(A[i][p]) > Math.abs(A[max][p])) {
+                if (abs(mainMatrix[i][p]) > abs(mainMatrix[max][p])) {//find largest absolute value for pivot
                     max = i;
                 }
             }
-            double[] temp = A[p]; A[p] = A[max]; A[max] = temp;
-            double   t    = b[p]; b[p] = b[max]; b[max] = t;
+            double[] temp = mainMatrix[p]; mainMatrix[p] = mainMatrix[max]; mainMatrix[max] = temp;
+            double   t;
+            t = rightSide[p];
+            rightSide[p] = rightSide[max]; rightSide[max] = t;
 
-            // singular or nearly singular
-            if (Math.abs(A[p][p]) <= EPSILON) {
-                throw new ArithmeticException("Matrix is singular or nearly singular");
-            }
-
-            // pivot within A and b
+            // pivot within A
             for (int i = p + 1; i < n; i++) {
-                double alpha = A[i][p] / A[p][p];
-                b[i] -= alpha * b[p];
+                double alpha = mainMatrix[i][p] / mainMatrix[p][p];
+                rightSide[i] -= alpha * rightSide[p];
                 for (int j = p; j < n; j++) {
-                    A[i][j] -= alpha * A[p][j];
+                    mainMatrix[i][j] -= alpha * mainMatrix[p][j];
                 }
             }
         }
@@ -37,31 +79,11 @@ public class Main {
         for (int i = n - 1; i >= 0; i--) {
             double sum = 0.0;
             for (int j = i + 1; j < n; j++) {
-                sum += A[i][j] * x[j];
+                sum += mainMatrix[i][j] * x[j];
             }
-            x[i] = (b[i] - sum) / A[i][i];
+            x[i] = (rightSide[i] - sum) / mainMatrix[i][i];
         }
         return x;
-    }
-
-
-    // sample client
-    public static void main(String[] args) {
-        int n = 3;
-        double[][] A = {
-                { 0, 1,  1 },
-                { 2, 4, -2 },
-                { 0, 3, 15 }
-        };
-        double[] b = { 4, 2, 36 };
-        double[] x = lsolve(A, b);
-
-
-        // print results
-        for (int i = 0; i < n; i++) {
-            System.out.println(x[i]);
-        }
-
     }
 
 }
